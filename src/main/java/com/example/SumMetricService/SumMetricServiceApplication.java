6ee130d924717b6c8fd05bcc0a4fecdd1f7a1ab8
rpatel1291 <rpatel1291@gmail.com>
2020-05-31 13:10:48 -0400

    Initial project folder structure. Created using https://start.spring.io/ and added Spring boot starter, Lombok, and h2

---
+++ src/main/java/com/example/SumMetricService/SumMetricServiceApplication.java
@@ -0,0 +1,14 @@
+package com.example.SumMetricService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SumMetricServiceApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SumMetricServiceApplication.class, args);
+	}
+
+}
